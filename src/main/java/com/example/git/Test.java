30cbf170514d7bf3496fa0af7e49279e72284f4b
Teetirat (Pai) Veangngoen <teetirat.vea@truedigital.com>
2021-11-08 13:31:26 +0700

    fix(c1): add new changelog
    Jira: PRF-123

--- src/main/java/com/example/git/Test.java
+++ src/main/java/com/example/git/Test.java
@@ -6,7 +6,7 @@ public class Test {
         System.out.println("231");
 
         System.out.println("3123");
-        System.out.println("dassad");
+        System.out.println("dassadasd");
 
     }
 }
